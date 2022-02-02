a8729a04cf6a090461879ecbb248e9cd4e2a809d
Patrick Huber <182398+patrickhuber@users.noreply.github.com>
2022-02-01 20:12:20 -0500

    adds dictionary

---
+++ dictionary/dictionary.go
@@ -0,0 +1,53 @@
+package dictionary
+
+type Dictionary interface {
+	Get(interface{}) interface{}
+	Set(interface{}, interface{})
+	Length() int
+	Clear()
+	Keys() []interface{}
+	Values() []interface{}
+}
+type dictionary struct {
+	values map[interface{}]interface{}
+}
+
+func New() Dictionary {
+	return &dictionary{
+		values: map[interface{}]interface{}{},
+	}
+}
+
+func (d *dictionary) Get(key interface{}) interface{} {
+	return d.values[key]
+}
+
+func (d *dictionary) Set(key interface{}, value interface{}) {
+	d.values[key] = value
+}
+
+func (d *dictionary) Length() int {
+	return len(d.values)
+}
+
+func (d *dictionary) Clear() {
+	for k := range d.values {
+		delete(d.values, k)
+	}
+}
+
+func (d *dictionary) Keys() []interface{} {
+	keys := []interface{}{}
+	for k := range d.values {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func (d *dictionary) Values() []interface{} {
+	values := []interface{}{}
+	for _, v := range d.values {
+		values = append(values, v)
+	}
+	return values
+}
